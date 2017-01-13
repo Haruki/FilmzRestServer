1fc14eaa08287f414750a3b640c7445467147631
pimpelkram <hb@hb.gl>
2017-01-13 07:19:03 +0100

    Saying beispiel deleted. id umgestellt auf pathParam ("filmz/1")

--- src/main/java/gl/hb/filmzrestserver/resources/FilmzResource.java
+++ src/main/java/gl/hb/filmzrestserver/resources/FilmzResource.java
@@ -2,21 +2,16 @@ package gl.hb.filmzrestserver.resources;
 
 import com.codahale.metrics.annotation.Timed;
 import gl.hb.filmzrestserver.api.Film;
-import gl.hb.filmzrestserver.api.Saying;
 import gl.hb.filmzrestserver.dao.FilmzDao;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.Optional;
-import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Created by Homer on 08.01.2017.
  */
-@Path("/filmz")
+@Path("/filmz/{movieid}")
 @Produces(MediaType.APPLICATION_JSON)
 public class FilmzResource {
     private final FilmzDao filmzDao;
@@ -27,7 +22,7 @@ public class FilmzResource {
 
     @GET
     @Timed
-    public Film sayHello(@QueryParam("id") Optional<Integer> id) {
+    public Film sayHello(@PathParam("movieid") Optional<Integer> id) {
         String nameDeutsch = filmzDao.findFilmById(id.orElse(1));
         return new Film(nameDeutsch);
     }
