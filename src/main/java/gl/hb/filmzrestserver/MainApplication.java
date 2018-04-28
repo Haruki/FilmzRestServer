b589ae4e40775005ec5321fa6294f837e794f046
pimpelkram <hb@hb.gl>
2018-04-28 15:50:13 +0200

    update

--- src/main/java/gl/hb/filmzrestserver/MainApplication.java
+++ src/main/java/gl/hb/filmzrestserver/MainApplication.java
@@ -14,10 +14,15 @@ import io.dropwizard.jdbi.DBIFactory;
 import io.dropwizard.jdbi.bundles.DBIExceptionsBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
 import org.skife.jdbi.v2.DBI;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import java.util.EnumSet;
 
 /**
  * Created by Homer on 08.01.2017.
@@ -45,6 +50,20 @@ public class MainApplication extends Application<FilmzRestServerConfiguration> {
     @Override
     public void run(FilmzRestServerConfiguration configuration,
                     Environment environment) {
+
+
+        // Enable CORS headers (from Stackoverflow):
+        final FilterRegistration.Dynamic cors =
+                environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+        // Configure CORS parameters
+        cors.setInitParameter("allowedOrigins", "*");
+        cors.setInitParameter("allowedHeaders", "*");
+        //cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
+        cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+        // Add URL mapping
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+
+
         //setup database:
         final DBIFactory factory = new DBIFactory();
         final DBI jdbi = factory.build(environment, configuration.getDataSourceFactory(), "oracle");
