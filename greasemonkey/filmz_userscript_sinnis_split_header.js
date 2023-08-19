afd866422468a5b46a244c5a47b7b82a2f5ba5a2
Haruki <hb@pimpelkram.com>
2023-08-19 22:48:12 +0200

    updated tampermonkey header: replacing deprecated include with match

--- greasemonkey/filmz_userscript_sinnis_split_header.js
+++ greasemonkey/filmz_userscript_sinnis_split_header.js
@@ -1,18 +1,19 @@
 // ==UserScript==
-// @name		Greasemonkey Bestfilmz (dropwizard server) v1
+// @name		Greasemonkey Bestfilmz (dropwizard server) v2
+// @version 3.0
 // @namespace 	http://www.oreilly.com/catalog/greasemonkeyhacks/
 // @description	BF Greasemonkey Client v1
-// @include        http://www.imdb.com/title/tt*/*
-// @include        https://www.imdb.com/title/tt*/*
-// @include        http://imdb.com/title/tt*
-// @include        https://imdb.com/title/tt*
+// @match        http://www.imdb.com/title/tt*/*
+// @match        https://www.imdb.com/title/tt*/*
+// @match        http://imdb.com/title/tt*
+// @match        https://imdb.com/title/tt*
 // @exclude         http://*.media-imdb.com/*
 // @exclude			http://www.imdb.com/rt/*
 // @exclude 		http://www.imdb.com/images/*
 // @exclude			http://www.imdb.com/widget/*
 // @exclude			http://www.imdb.com/title/tt*/_ajax/*
 // @exclude 		http://www.imdb.com/title/tt*//event/*
-// @require         file:///C:/Users/Asterix/dotfiles/greasemonkey/filmz_userscript_sinnis_split.js
+// @require         file:///D:/coding/FilmzRestServer/greasemonkey/filmz_userscript_sinnis_split.js
 // @grant			GM_xmlhttpRequest
 // @grant           GM_setValue
 // @grant           GM_getValue
